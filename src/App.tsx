66cac9161f23cbf9d65b2948ec0b1c077cfd81f2
cahya_agung <tjahja_lol@yahoo.com>
2023-06-29 20:12:18 +0700

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import './assets/font.css';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, HashRouter } from 'react-router-dom';
 import { AnimeList, AnimeDetail, Collection, CollectionDetail } from './pages';
 
 function App() {
   const basename = process.env.REACT_APP_BASENAME || '';
 
   return (
-    <BrowserRouter basename={basename}>
+    //<BrowserRouter basename={basename}>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<AnimeList />} />
         <Route path='/anime/:id' element={<AnimeDetail />} />
         <Route path='/collections' element={<Collection />} />
         <Route path='/collections/:name' element={<CollectionDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    //</BrowserRouter>
   );
 }
 
